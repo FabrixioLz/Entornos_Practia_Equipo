a799cc40569e9020292eb8f3c3734e371724d6f5
MataOtakus <konradwachowski1@gmail.com>
2025-04-08 15:46:16 +0200

    He añadido la clase Producto y eh actualizado las clases Usuario, Proovedor y Producto, añadi los constructores y getter y setter.

--- Entornos_Practia_Proyecto/src/modelo/Usuario.java
+++ Entornos_Practia_Proyecto/src/modelo/Usuario.java
@@ -1,5 +1,38 @@
 package modelo;
 
 public class Usuario {
+	private String user;
+	public enum Roll{TRABAJADOR, GERENTE}
+	private Roll roll;
+	private String pass;
+	
+	public Usuario(String user, Roll roll, String pass) {
+		this.user = user;
+		this.roll = roll;
+		this.pass = pass;
+	}
 
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public Roll getRoll() {
+		return roll;
+	}
+
+	public void setRoll(Roll roll) {
+		this.roll = roll;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
 }
--- Entornos_Practia_Proyecto/src/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Entornos_Practia_Proyecto {
-}
